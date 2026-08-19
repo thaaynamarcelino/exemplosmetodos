9eace939d4ed311ebdad1792f4590d51b6e31a01
Thayná Marcelino <thaaynamarcelino@gmail.com>
2026-08-19 20:49:36 -0300

    Create atv5.java

---
+++ atv5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class atv5{
+
+public static int somar ( int a, int b) {
+return a + b;
+}
+public static void main(String[] args) {
+int resultado = somar(20,10);
+System.out.println(resultado);
+
+
+   }
