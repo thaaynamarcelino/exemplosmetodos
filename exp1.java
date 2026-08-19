3076825df2ea19146ecedf7db3d2f8b9a3b8db93
Thayná Marcelino <thaaynamarcelino@gmail.com>
2026-08-19 19:31:51 -0300

    Add Main class with greeting message method

---
+++ exp1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+
+     public static void mostrarMensagem() {
+       System.out.println("Olá, aluno!");
+    }
+    public static void main(String[] args) {
+    mostrarMensagem();
+    }
+}
