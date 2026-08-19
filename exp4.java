a7ac6abaab00720a37a6bdf46944b2f8d188d791
Thayná Marcelino <thaaynamarcelino@gmail.com>
2026-08-19 20:14:35 -0300

    Add greeting function and main method

---
+++ exp4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+public class Main{
+
+public static void  saudacao (
+String nome) {
+System.out.println("olá," + nome );
+
+}
+    public static void main(String[] args) {
+      saudacao("joao");
+  saudacao("lucas");
+  saudacao("victor");
+
+   }
+
+  }
+  
