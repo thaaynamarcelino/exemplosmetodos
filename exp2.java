0089e5ed5b7195a78990f2917e6dd1663428f043
Thayná Marcelino <thaaynamarcelino@gmail.com>
2026-08-19 19:33:34 -0300

    Add aluno method to print student details

---
+++ exp2.java
@@ -0,0 +1,11 @@
+Public static void aluno(
+String nome,
+int idade,
+double nota) {
+  
+System.out.println("Nome: " + nome);
+System.out.println("Idade: " + idade);
+System.out.println("Nota: " + nota);
+}
+
+aluno("Maria", 20, 8.5);
