1369f87e5f4cec4eecf0140dc7521bcb86e33510
Thayná Marcelino <thaaynamarcelino@gmail.com>
2026-08-19 21:13:01 -0300

    Add average calculation in exp6.java
    Implement a method to calculate the average of two grades and display the result.

---
+++ exp6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class exp6{
+
+public static double calcularMedia  ( double nota1, double nota2) {
+return nota1 + nota2;
+}
+
+
+public static void main(String[] args) {
+
+double calculo = calcularMedia(8,6);
+double media = calculo / 2 ;
+
+System.out.println(media);
+
+
+
+   }
+
+}
+  
+  
